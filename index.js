24f101b93c2a05bad16cf3d0e82d61dc8f583e43
pooja201095 <poojachavan153@gmail.com>
2022-09-11 17:51:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req,res){
 })
 
 
-var server = app.listen(3003,function(){
+var server = app.listen(3000,function(){
     var host = server.address().address;
     var port = server.address().port;
 
